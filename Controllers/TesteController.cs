da7071d8f6f64fbb896be977439de4a65f8368a3
Ryan Lirio <ryanlirio2@gmail.com>
2025-03-17 21:58:43 -0300

    finalizado

--- Controllers/TesteController.cs
+++ Controllers/TesteController.cs
@@ -13,7 +13,13 @@ namespace WebApplication1.Controllers
 
     public class TesteController : Controller
     {
-        string palavra;
+        string [] palavra;
+        string palavracodificada;
+        char[] alfabeto = new char[]
+        {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        };
+
 
         private readonly ILogger<TesteController> _logger;
 
@@ -37,14 +43,50 @@ namespace WebApplication1.Controllers
         public IActionResult Index(string texto)
         {
             Result resultado = new();
-            resultado.Texto = texto.ToUpper();
-            
-            for(int i = 0; i <= texto.Length; i++)
-            {
-                palavra = texto.Substring(0, 1);
+             resultado.Texto = texto.ToUpper();
 
+            palavra = new string[texto.Length];
+            palavracodificada = "";
+
+            for (int i = 0; i < texto.Length; i++)
+            {
+                palavra[i] = texto.Substring(i, 1);
             }
-            resultado.Texto = palavra;
+
+            for(int i = 0;i < palavra.Length; i++)
+            {
+                char letraOriginal = char.Parse(palavra[i]);
+
+                if (char.IsLetter(letraOriginal))
+                {
+                    
+                    int posicaoOriginal = Array.IndexOf(alfabeto, char.ToLower(letraOriginal));
+
+                    
+                    int novaPosicao = (posicaoOriginal + 3) % alfabeto.Length;
+
+                    
+                    char novaLetra = alfabeto[novaPosicao];
+
+                    
+                    if (char.IsUpper(letraOriginal))
+                    {
+                        palavracodificada += char.ToUpper(novaLetra);
+                    }
+                    else
+                    {
+                        palavracodificada += novaLetra;
+                    }
+                }
+                else
+                {
+                    
+                    palavracodificada += letraOriginal;
+                }
+            }
+
+            
+            resultado.Texto = palavracodificada;
 
             return View("Index", resultado);
         }
